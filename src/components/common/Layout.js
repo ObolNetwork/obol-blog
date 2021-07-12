c0c7d54f3d279ee16d09dfadb138aef1137a9670
Oisín Kyne <oisin@kyne.eu>
2021-07-12 17:51:55 +0100

    FIx title image

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -54,7 +54,8 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                             </div>
                             { isHome ?
                                 <div className="site-banner">
-                                    <h1 className="site-banner-title">{site.title}</h1>
+                                    {/* <h1 className="site-banner-title">{site.title.toUpperCase()}</h1> */}
+                                    <div className="site-banner-logo" />
                                     <p className="site-banner-desc">{site.description}</p>
                                 </div> :
                                 null}
